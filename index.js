c7e7b2e9f71b48af3ddbea1e625c9004411851f9
thorlio <thorlio3@gmail.com>
2024-12-07 14:48:36 -0800

    Updated files
    Added new MongoDB connection

--- index.js
+++ index.js
@@ -15,13 +15,49 @@ const Users = Models.User;
 const port = process.env.PORT || 8080;
 
 // Database connection
-mongoose.connect('mongodb://localhost:27017/myNewDatabase')
+// mongoose.connect('mongodb://localhost:27017/myNewDatabase')
+// .then(() => {
+//   console.log('Connected to MongoDB');
+// })
+// .catch((err) => { 
+//   console.log('Could not connect to MongoDB:', err);
+// });
+
+mongoose.connect('mongodb+srv://thorlio3:PUXWVUhLT85ew3Pi@cluster0.1sglm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => {
   console.log('Connected to MongoDB');
 })
 .catch((err) => { 
   console.log('Could not connect to MongoDB:', err);
 });
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://thorlio3:<db_password>@cluster0.1sglm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
 
 // Middleware
 
@@ -318,7 +354,6 @@ app.delete('/movies/:title', passport.authenticate('jwt', { session: false }), (
 app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port ' + port);
 });
-
 
 
 
