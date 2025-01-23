1bc1108d5f566731ef6ffe30c4c3e760c1122e7c
thorlio <thorlio3@gmail.com>
2025-01-23 01:45:16 -0800

    Updated MongoDB URI connection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
@@ -17,17 +18,39 @@ const jwt = require("jsonwebtoken");
 //   useUnifiedTopology: true,
 // });
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB!");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri =
+//   "mongodb+srv://thorlio3:Sacramento%408@cluster0.1sglm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// mongoose
+//   .connect(process.env.MONGODB_URI, { serverSelectionTimeoutMS: 30000 })
+//   .then(() => console.log("Connected to MongoDB!"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
 const allowedOrigins = [
   "http://localhost:3000",
